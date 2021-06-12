ff1028206c402402771edd0065db438e36027e21
MykolaV <kolya.vasevich@gmail.com>
2021-06-12 16:41:29 +0300

    added BSON attributes to models

--- Bot.Data/Models/Mute.cs
+++ Bot.Data/Models/Mute.cs
@@ -1,7 +1,12 @@
-﻿namespace Bot.Data.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Bot.Data.Models
 {
     public class Mute
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public int Id { get; set; }
         public int UserId { get; set; }
         public int ChatId { get; set; }
--- Bot.Data/Models/Settings.cs
+++ Bot.Data/Models/Settings.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Bot.Data.Models
 {
     public class Settings
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public int Id { get; set; }
         public int ChatId { get; set; }
         public IList<int> RestrictedWords { get; set; }
